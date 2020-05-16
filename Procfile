af43e6237cdf93117881a5d87924a0bac40c6700
Carlos Bonfim <carlos_bonfim10@hotmail.com>
2020-05-16 14:28:39 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,5 @@
+﻿import os
+with open(os.path.join('app.py','Procfile'), "w") as file1:
+    toFile = 'web: sh setup.sh && streamlit run app.py'
+    
+file1.write(toFile)
---
+++ setup.sh
@@ -0,0 +1,11 @@
+﻿mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"carlos_bonfim10@hotmail.com"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
